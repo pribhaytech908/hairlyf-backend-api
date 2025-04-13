5bf354711c2fdc291c9e526d9f2a58eda3eee508
pribhaytech908 <sdhirajkumar490@gmaail.com>
2025-04-13 14:47:26 +0530

    cors issue fixed

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ config(); // Load .env
 connectDB(); // Connect MongoDB
 
 const app = express();
-
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 // Middlewares
 app.use(cors());
 app.use(json());
