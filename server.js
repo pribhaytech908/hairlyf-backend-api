9fbe385e9aa252915bcf5e7c1c2ed231966afce5
pribhaytech908 <pribhaytech@gmail.com>
2025-06-11 21:11:55 +0530

    patch included in cors

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const corsOptions = {
     'https://main.d3tqy4xj2yse0u.amplifyapp.com'
   ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
